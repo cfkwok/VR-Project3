b51beea38a9ef34007301c3555dc78ee88adb19c
Timothy Russell-Wagner <atonement100@gmail.com>
2016-03-23 21:17:49 -0400

    Player hud, trigger system to update hud
    Hook Time/Weather/Trail(Text) up to timeText(Text) etc. and put in
    Time/Weather/Trail (string inputs) what you want to be changed. Empty
    input for strings will result in no change.

---
+++ VRMountEverest/Assets/PlayerUpdate.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerUpdate : MonoBehaviour {
+    public Text timeText, weatherText, trailText;
+    public string time, weather, trail;
+    
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (time != "") { timeText.text = time; }
+            if (weather != "") { weatherText.text = weather; }
+            if (trail != "") { trailText.text = trail; }
+        }
+    }
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
