cd62bd886f6976507fdacf6c7046aa793a706346
cfkwok <cfkwok@ufl.edu>
2016-03-27 15:09:14 -0400

    Adding paths

--- VRMountEverest/Assets/_Scripts/updateElevation.cs
+++ VRMountEverest/Assets/_Scripts/updateElevation.cs
@@ -9,8 +9,13 @@ public class updateElevation : MonoBehaviour {
     private double elev;
 	
 	void Update () {
-        elev = 29029.0 - (peak.transform.position.y-transform.position.y)*3.28084;
-        elev = Math.Round(elev, 1);
+		// South col math
+		// Start = 17k, end = 29025, diff = 12025
+		// unity: start = 254 end = 562, diff = 308
+		// we elevate at a speed of 39.04 ft/unit
+        // elev = 29029.0 - (peak.transform.position.y-transform.position.y)*3.28084;
+        elev = 17000 + (transform.position.y - 254) * 39.04;
+        elev = Math.Round(elev) - (Math.Round(elev) % 10);
         elevation.text = elev.ToString() + " feet";
 	}
 }
