ede82b8d9724f377c51ea34cdaceaff56d9410ff
Timothy Russell-Wagner <atonement100@gmail.com>
2016-03-27 13:47:28 -0400

    updated hud prefab, set up with automatic elev update

--- VRMountEverest/Assets/_Scripts/PlayerUpdate.cs
+++ VRMountEverest/Assets/_Scripts/PlayerUpdate.cs
@@ -3,8 +3,8 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerUpdate : MonoBehaviour {
-    public Text timeText, weatherText, trailText;
-    public string time, weather, trail;
+    public Text timeText, weatherText, trailText, elevationText;
+    public string time, weather, trail, elevation;
     
 
 	// Use this for initialization
@@ -19,6 +19,7 @@ public class PlayerUpdate : MonoBehaviour {
             if (time != "") { timeText.text = time; }
             if (weather != "") { weatherText.text = weather; }
             if (trail != "") { trailText.text = trail; }
+            if (elevation != "") { trailText.text = elevation; }
         }
     }
 
---
+++ VRMountEverest/Assets/_Scripts/updateElevation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class updateElevation : MonoBehaviour {
+    public GameObject peak;
+    public Text elevation;
+    private double elev;
+	
+	void Update () {
+        elev = 29029.0 - (peak.transform.position.y-transform.position.y)*3.28084;
+        elev = Math.Round(elev, 1);
+        elevation.text = elev.ToString() + " feet";
+	}
+}
