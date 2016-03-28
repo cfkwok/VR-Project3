82beec7a7180394176b342ac62786bfff7b4c092
cfkwok <cfkwok@ufl.edu>
2016-03-28 19:11:45 -0400

    Add breathing scale of elevation

--- VRMountEverest/Assets/_Scripts/updateElevation.cs
+++ VRMountEverest/Assets/_Scripts/updateElevation.cs
@@ -8,6 +8,8 @@ public class updateElevation : MonoBehaviour {
     public WeatherController weatherController;
     public Text elevation;
     public double elev;
+    public AudioSource breathingAud;
+    private int elevationLevel = 0;
 	
 	void Update () {
 		// South col math
@@ -19,5 +21,47 @@ public class updateElevation : MonoBehaviour {
         elev = Math.Round(elev) - (Math.Round(elev) % 10);
         String temp = weatherController.currentWeather.temp.ToString();
         elevation.text = elev.ToString() + " feet, " + temp + "°F";
+        if (elev > 27000) {
+            
+            if (elevationLevel != 3) {
+                breathingAud.Stop();
+                breathingAud.volume = 0.013f;
+                breathingAud.Play();
+                elevationLevel = 3;
+            }
+
+            
+        }
+        else if (elev > 24000) {
+            
+            if (elevationLevel != 2) {
+                breathingAud.Stop();
+                breathingAud.volume = 0.011f;
+                breathingAud.Play();
+                elevationLevel = 2;
+            }
+            
+        }
+        else if (elev > 21000) {
+            
+            if (elevationLevel != 1) {
+                breathingAud.Stop();
+                breathingAud.volume = 0.009f;
+                breathingAud.Play();
+                elevationLevel = 1;
+            }
+            
+        }
+        else {
+            
+            if (elevationLevel != 0) {
+                breathingAud.Stop();
+                breathingAud.volume = 0.003f;
+                breathingAud.Play();
+                elevationLevel = 0;
+            }
+            
+        }
+
 	}
 }
