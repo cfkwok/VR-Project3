ca62992c27d20db7189cc21581e9dc597f71ebd5
Timothy Russell-Wagner <atonement100@gmail.com>
2016-03-28 15:27:25 -0400

    Targeting update script
    incomplete, pushing to be looked at while at work. Takes in gameObjects
    Target, Player, and Image. Target is destination, player is object that
    will always be facing the same direction as player, image is the image
    on the hud to be rotated.

---
+++ VRMountEverest/Assets/_Scripts/targetingUpdate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class targetingUpdate : MonoBehaviour {
+    public GameObject target, player, image;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var angle = Vector3.Angle(player.transform.forward, target.transform.position - player.transform.position);
+        print(angle);
+        image.transform.localRotation = Quaternion.Euler(0, 0, (float)angle);       
+	}
+}
