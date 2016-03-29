c97e484aaa35eaa0e640b1b5e9cf7055a9b35cd0
Timothy Russell-Wagner <atonement100@gmail.com>
2016-03-29 13:59:54 -0400

    targeting update complete

--- VRMountEverest/Assets/_Scripts/targetingUpdate.cs
+++ VRMountEverest/Assets/_Scripts/targetingUpdate.cs
@@ -4,8 +4,6 @@ using System.Collections;
 
 public class targetingUpdate : MonoBehaviour {
     public GameObject target, player, image;
-
-
 	// Use this for initialization
 	void Start () {
 	
@@ -13,8 +11,9 @@ public class targetingUpdate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        var angle = Vector3.Angle(player.transform.forward, target.transform.position - player.transform.position);
-        print(angle);
+        var angle = Vector3.Angle(Camera.main.transform.forward, target.transform.position - player.transform.position);
+        Vector3 targetinvt = player.transform.InverseTransformPoint(target.transform.position);
+        if ( targetinvt.x > 0) { angle = -angle; }
         image.transform.localRotation = Quaternion.Euler(0, 0, (float)angle);       
-	}
+	}    
 }
