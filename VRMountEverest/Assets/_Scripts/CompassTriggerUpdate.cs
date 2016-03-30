2c87bac435907773ee5c5712180243000453b77a
cfkwok <cfkwok@ufl.edu>
2016-03-29 22:15:12 -0400

    Add trigger update to arrow direction

---
+++ VRMountEverest/Assets/_Scripts/CompassTriggerUpdate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompassTriggerUpdate : MonoBehaviour {
+	public targetingUpdate tUpdate;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		tUpdate.target = GameObject.Find("Cube2");
+	}
+}
