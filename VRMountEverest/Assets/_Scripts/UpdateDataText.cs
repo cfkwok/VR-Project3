5edc9c1bfb641570a267b43fb3a3ad58f8a604cf
Tonny <tonnyx1294@hotmail.com>
2016-03-31 04:30:05 -0400

    added last animation/model

--- VRMountEverest/Assets/_Scripts/UpdateDataText.cs
+++ VRMountEverest/Assets/_Scripts/UpdateDataText.cs
@@ -27,16 +27,16 @@ public class UpdateDataText : MonoBehaviour {
 			dataText.text = "The second most common starting point of summiters is the North Col with at least 1900 summiters!";
 		} else if (other.gameObject.name == "Falling") {
 			// 68 deaths by fall
-			dataText.text = "Cause of Death: Falling | 68 Recorded Deaths";
+			dataText.text = "Cause of Death: Falling |         68 Recorded Deaths";
 		} else if (other.gameObject.name == "Avalanche") {
 			// 47 deaths by avalanche
 			dataText.text = "Cause of Death: Avalanche | 47 Recorded Deaths";
 		} else if (other.gameObject.name == "Frostbite") {
 			// 24 deaths by exposure/frostbite
-			dataText.text = "Cause of Death: Exposure/Frostbite | 24 Recorded Deaths ";
+			dataText.text = "Cause of Death: Frostbite |   24 Recorded Deaths ";
 		} else if (other.gameObject.name == "Exhaustion") {
 			// 13 deaths by exhaustion
-			dataText.text = "Cause of Death: Exhaustion| 13 Recorded Deaths ";
+			dataText.text = "Cause of Death: Exhaustion|   13 Recorded Deaths ";
 		} else if (other.gameObject.name == "Peak") {
 			// 5113 summiters; 219 deaths
 			dataText.text = "Congratulations! You are the 5114th summiter of 5332 attempts!";
